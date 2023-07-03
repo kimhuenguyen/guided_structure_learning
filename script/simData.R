bcd9a0463da57478c85ae4aa59022a882bc4eb76
kimhuenguyen <31039574+kimhuenguyen@users.noreply.github.com>
2023-07-03 21:02:56 +0200

    Update simData.R

--- script/simData.R
+++ script/simData.R
@@ -9,16 +9,14 @@
 #' @importFrom
 #' @examples
 #' p <- 4
-#' Pmat <- matrix(c(0, 0.4, -0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.3, 0, 0, 0), nrow=p,byrow=T)
-#' n <- 10
 #' order <- seq(1,p,1)
-#' simData(n, p, order,Pmat)
+#' Pmat <- matrix(c(0, 0.4, -0.2, 0, 0, 0, 0, 0.3, 0, 0, 0, 0, 0, 0, 0, 0), nrow=p,byrow=T)
+#' n <- 100
+#' simData.DAG(n, p, order,Pmat)
 #' @export
-simData <- function(n,p,order,Pmat){
+simData.DAG <- function(n,p,order,Pmat){
   all.simulated <- FALSE
   data <- matrix(0,nrow = n,ncol = p)
-  cmax <- apply(abs(Pmat),2,max)
-  temp <- which(cmax == 0)
   while(all.simulated == FALSE ){
     for (i in 1: p){
       if (sum(abs(Pmat[,order[i]]))==0)
